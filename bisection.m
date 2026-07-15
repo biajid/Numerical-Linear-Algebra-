dd6af2f2352ec5e37ba91ad37f3459a92a1030f7
biajid <bec24@njit.edu>
2026-07-15 16:24:30 -0400

    . added

--- bisection.m
+++ bisection.m
@@ -10,27 +10,42 @@ if xa * xb > 0
     error('Bracketing method would not work because both guess is in the same side!');
 end
 
+if (xa < 0)
+    dummy = b;
+    b = a;
+    a = dummy;
+
+    dummy = xb;
+    xb = xa;
+    xa = dummy;
+end
+
+
 xns = (a + b) / 2;
 c = fun(xns);
 
 
-toli = (b - a) / 2;
+toli = abs(b - a) / 2;
 
 while toli > tolerance
-    if xa * c > 0
-        a = xns;
-        xa = fun(a);
-    else 
-        b = xns;
-        xb = fun(b);
+    if c > 0
+       a = xns;
+       xa = c;
+    else
+       b = xns;
+       xb = c;
     end
 
     xns = (a + b) / 2;
     c = fun(xns);
+    
+    if c == 0
+        return;
+    end
 
     
 
-    toli = (b - a) / 2;
+    toli = abs(b - a) / 2;
 end
 
 end
